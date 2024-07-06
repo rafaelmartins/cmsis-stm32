3fb8838239d132476899e29e0bc662cc38378932
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2024-07-07 00:47:29 +0200

    new package: cmsis-stm32c0

---
+++ packages/cmsis-stm32c0.sh
@@ -0,0 +1,23 @@
+version=1.2.0
+description="CMSIS headers for STM32C0 Series of ARM Cortex-M0+ microcontrollers"
+license=Apache-2.0
+github_repository=STMicroelectronics/STM32CubeC0
+
+family_definition=STM32C0xx
+
+device_definitions=(
+    STM32C011xx
+    STM32C031xx
+    STM32C071xx
+)
+
+cmsis_headers=(
+    core_cm0plus.h
+    cmsis_version.h
+    cmsis_compiler.h
+    cmsis_gcc.h
+    mpu_armv7.h
+)
+
+cpu=cortex-m0plus
+fpu=
